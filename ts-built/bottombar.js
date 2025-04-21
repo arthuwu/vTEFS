d6352364c14512bbfa1f744d3b28a0502d10bf9b
arthur <89831445+arthuwu@users.noreply.github.com>
2025-04-21 12:13:38 +0100

    fix: delete ts-built directory

--- ts-built/bottombar.js
+++
@@ -1,8 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import "./bottombar.css";
-function createNewStrip() {
-    console.log("h1");
-}
-export default function BottomBar() {
-    return (_jsx("div", { className: "bottom-bar", children: _jsx("button", { onClick: createNewStrip, children: "Create new" }) }));
-}
